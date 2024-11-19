4ce6145a16dc63ab7028c1b9f1436cad8832ac86
guve760204 <114717483+guve760204@users.noreply.github.com>
2024-11-18 21:57:57 -0600

    corregido el admin guard

--- app/Models/User.php
+++ app/Models/User.php
@@ -48,4 +48,8 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function refresh_tokens(){
+        return $this->hasMany(RefreshToken::class);
+    }
 }
